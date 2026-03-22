34b4bcba739eb2728fbcd2f5252aefca882f7b4f
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2026-03-22 20:09:13 +0100

    extract table function

--- PSET1_micrometrics.r
+++ PSET1_micrometrics.r
@@ -20,54 +20,6 @@ source("./setup.R")
 
 # a)
 # Create table to check balance across treatment groups for some covariates
-
-# create function, as it'll be used again in task 2
-Table_original_function <- function(data, vars, treat_var) {
-  tvar <- data[[treat_var]]
-
-  n1 <- sum(tvar == 1)
-  n0 <- sum(tvar ==0)
-
-  table_m <- matrix(NA, nrow = 7, ncol = length(vars))
-  rownames(table_m) <- c(
-    "Mean treated",
-    "Mean control",
-    "SD treated",
-    "SD control",
-    "Diff. in means",
-    "SE",
-    "p-value"
-  )
-  colnames(table_m) <- vars
-
-  for (i in seq_along(vars)) {
-    target <- data[[vars[i]]]
-
-    treated <- target[tvar==1]
-    control <- target[tvar==0]
-    treated_mean <- mean(treated)
-    control_mean <- mean(control)
-    treated_sd <- sd(treated)
-    control_sd <- sd(control)
-    diff_in_means <- treated_mean - control_mean
-    SE_diff_in_means <- sqrt(treated_sd^2/n1+control_sd^2/n0)
-    p_val <- t.test(x = treated, y = control, alternative = "two.sided", var.equal = FALSE)$p.value
-
-    table_m[ ,i] <- c(treated_mean,
-                      control_mean,
-                      treated_sd,
-                      control_sd,
-                      diff_in_means,
-                      SE_diff_in_means,
-                      p_val
-    )
-  }
-  table_m <- as.data.frame(table_m)
-  table_m[] <- round(table_m, 3)
-
-  return(table_m)
-}
-
 Table_1 <- Table_original_function(jtrain2,
                         vars = c("age","educ","black","hisp","nodegree","re74","re75"),
                         treat_var = "train"
--- setup.R
+++ setup.R
@@ -38,3 +38,49 @@ reg_extractor <- function(linmod, treat = "train") {
   p_value_mod <- coeffs_gen[treat, "Pr(>|t|)"]
   c(n1_new = n1_new, n0_new = n0_new, Estimate = coef_mod, SE = se_mod, p_value = p_value_mod)
 }
+
+Table_original_function <- function(data, vars, treat_var) {
+  tvar <- data[[treat_var]]
+
+  n1 <- sum(tvar == 1)
+  n0 <- sum(tvar ==0)
+
+  table_m <- matrix(NA, nrow = 7, ncol = length(vars))
+  rownames(table_m) <- c(
+    "Mean treated",
+    "Mean control",
+    "SD treated",
+    "SD control",
+    "Diff. in means",
+    "SE",
+    "p-value"
+  )
+  colnames(table_m) <- vars
+
+  for (i in seq_along(vars)) {
+    target <- data[[vars[i]]]
+
+    treated <- target[tvar==1]
+    control <- target[tvar==0]
+    treated_mean <- mean(treated)
+    control_mean <- mean(control)
+    treated_sd <- sd(treated)
+    control_sd <- sd(control)
+    diff_in_means <- treated_mean - control_mean
+    SE_diff_in_means <- sqrt(treated_sd^2/n1+control_sd^2/n0)
+    p_val <- t.test(x = treated, y = control, alternative = "two.sided", var.equal = FALSE)$p.value
+
+    table_m[ ,i] <- c(treated_mean,
+                      control_mean,
+                      treated_sd,
+                      control_sd,
+                      diff_in_means,
+                      SE_diff_in_means,
+                      p_val
+    )
+  }
+  table_m <- as.data.frame(table_m)
+  table_m[] <- round(table_m, 3)
+
+  return(table_m)
+}
