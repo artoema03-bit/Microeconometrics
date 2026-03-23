6e28dfd9b8c21f8f0fa6346bcf912ac9524bf263
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2026-03-23 12:50:13 +0100

    rework task1

--- PSET1_micrometrics.r
+++ PSET1_micrometrics.r
@@ -20,68 +20,69 @@ source("./setup.R")
 
 # a)
 # Create table to check balance across treatment groups for some covariates
-Table_1 <- Table_original_function(jtrain2,
-                        vars = c("age","educ","black","hisp","nodegree","re74","re75"),
-                        treat_var = "train"
-                        )
+vars <- c("age", "educ", "black", "hisp", "nodegree", "re74", "re75")
+datasummary_balance(~ train , fmt = 3, data = jtrain2[, c("train", vars)], dinm_statistic = "p.value")
 
-# using stargazer as recommended by instructions
-# I am not including this line in the function itself to adapt it to queries included
-# in Task 2.
-stargazer(Table_1, summary = FALSE, type = "text", rownames = TRUE)
+# At a 5% significance level, only the difference in `nodegree` is statistically significant.
+# At a 10% significance level, also the difference in `hisp` is statistically significant.
+# At these significance levels we would expect to find only 1 in 20 and 1 in 10 significance differences, respectively, so there is some cause for concern on the sample being balanced.
+# However, given that this a smaller subset of a randomized sample, these imbalances could have also arisen due to bad luck.
 
 # b)
 # regress re78 on train, save estimate and SE of the coefficient as scalars
 
 regression <- lm(re78 ~ train, data = jtrain2)
-
 summary(regression)
 
 alpha <- coefficients(regression)["train"]
 beta  <- summary(regression)$coefficients["train", "Std. Error"]
 
+# The coefficient implies that participation in the training program is associated with an increase of 1.79 thousand 1982 dollars in 1978 real earnings , ceteris paribus.
+# The coefficient is significant at a 1% significance level.
+
 # c)
 # create new table to test covariate sensitivity
 
-TABLE_2_m <- matrix(NA, nrow = 5, ncol = 3)
-rownames(TABLE_2_m) <- c(
-  "Treated units",
-  "Control units",
-  "Treatment coefficient",
-  "Standard error",
-  "p-value"
-)
-colnames(TABLE_2_m) <- c(
-  "Reg_1",
-  "Reg_2",
-  "Reg_3"
+regs <- list(
+  regression_1 = lm(re78 ~ train, data = jtrain2),
+  regression_2 = lm(re78  ~ train + age + educ + black + hisp, data = jtrain2),
+  regression_3 = lm(re78  ~ train + age + educ + black + hisp + re74 + re75, data = jtrain2)
 )
 
-regression_2 <- lm(re78  ~ train + age + educ + black + hisp, data = jtrain2)
-regression_3 <- lm(re78  ~ train + age + educ + black + hisp + re74 + re75, data = jtrain2)
+get_group_counts <- \(model, treat = "train") {
+  mf <- model.frame(model)
+  if (!treat %in% names(mf)) stop("treatment variable not found in model frame")
 
-s1 <- reg_extractor(regression)
-s2 <- reg_extractor(regression_2)
-s3 <- reg_extractor(regression_3)
+  n1 <- sum(mf[[treat]] == 1, na.rm = TRUE)
+  n0 <- sum(mf[[treat]] == 0, na.rm = TRUE)
+  c(as.character(n1), as.character(n0))
+}
 
-TABLE_2_m[ ,1] <- s1
-TABLE_2_m[ ,2] <- s2
-TABLE_2_m[ ,3] <- s3
+rows <- data.frame(
+  "term" = c("N Treated ", "N Control "),
+  "Model 1" = get_group_counts(regs[[1]]),
+  "Model 2" = get_group_counts(regs[[2]]),
+  "Model 3" = get_group_counts(regs[[3]])
+)
 
-TABLE_2 <- as.data.frame(TABLE_2_m)
-# using stargazer as recommended by instructions
-stargazer(TABLE_2, summary = FALSE, type = "text", rownames = TRUE)
+modelsummary(
+  regs,
+  add_rows = rows,
+  gof_map = c("r.squared", "adj.r.squared", "nobs"),
+  stars = c('*' = .1, '**' = .05, '***' = 0.01),
+  title = "Task 1c"
+)
 
 # Are your results sensitive to the introduction of covariates?
-# The introduction of covariates slightly reduces the treatment effect, but it is
-#pretty stable overall. Similarly, the SE remains essentially unchanged after
-# adding the covariates. Furthermore the significance remains intact. All of these
-# point to the fact that our baseline estimate was not severy biased by ommitted variables
+# The introduction of covariates slightly reduced the treatment coefficient from 1.79 to 1.68 thousand 1982 dollars, but it is pretty stable overall.
+# Similarly, the SE marginally decreased, but remains essentially unchanged after adding the covariates.
+# So, the statistical significance of the results remains intact.
+# All of these point to the fact that our baseline estimate may not have been severy biased by ommitted variables.
 
 # d)
 # redoing reg_3 with dfbeta
 
-inflm_reg <- influence.measures(regression_3) # first subtask
+inflm_reg <- influence.measures(regs[[3]]) # first subtask
 influence_train <- inflm_reg$infmat[,"dfb.tran"] # no idea why its called that
 
 # second subtask (rerun regression)
@@ -98,54 +99,32 @@ t1 <- subsample_maker(dc=3)
 t2 <- subsample_maker(dc=5)
 t3 <- subsample_maker(dc=10)
 
-
-reg_3_1 <- lm(re78 ~ train + age + educ + black + hisp + re74 + re75, data = jtrain2[-t1,])
-reg_3_2 <- lm(re78 ~ train + age + educ + black + hisp + re74 + re75, data = jtrain2[-t2,])
-reg_3_3 <- lm(re78 ~ train + age + educ + black + hisp + re74 + re75, data = jtrain2[-t3,])
-
-
-# building table (for presentation purposes)
-TABLE_3_m <- matrix(NA, nrow = 5, ncol = 4)
-rownames(TABLE_3_m) <- c(
-  "Treated units",
-  "Control units",
-  "Treatment coefficient",
-  "Standard error",
-  "p-value"
-)
-colnames(TABLE_3_m) <- c(
-  "Reg_3_generic",
-  "Reg_3_drop3",
-  "Reg_3_drop5",
-  "Reg_3_drop10"
+regs_dfb <- list(
+  reg_3_generic = regs[[3]],
+  reg_3_drop3 = lm(re78 ~ train + age + educ + black + hisp + re74 + re75, data = jtrain2[-t1,]),
+  reg_3_drop5 = lm(re78 ~ train + age + educ + black + hisp + re74 + re75, data = jtrain2[-t2,]),
+  reg_3_drop10 = lm(re78 ~ train + age + educ + black + hisp + re74 + re75, data = jtrain2[-t3,])
 )
 
-s4 <- reg_extractor(reg_3_1)
-s5 <- reg_extractor(reg_3_2)
-s6 <- reg_extractor(reg_3_3)
-TABLE_3_m[ ,1] <- s3
-TABLE_3_m[ ,2] <- s4
-TABLE_3_m[ ,3] <- s5
-TABLE_3_m[ ,4] <- s6
+rows_dfb <- data.frame(
+  "term" = c("N Treated ", "N Control "),
+  "Model 1" = get_group_counts(regs_dfb[[1]]),
+  "Model 2" = get_group_counts(regs_dfb[[2]]),
+  "Model 3" = get_group_counts(regs_dfb[[3]]),
+  "Model 4" = get_group_counts(regs_dfb[[4]])
+)
 
-TABLE_3 <- as.data.frame(TABLE_3_m)
-TABLE_3[] <- round(TABLE_3, 3)
+modelsummary(
+  regs_dfb,
+  add_rows = rows_dfb,
+  gof_map = c("r.squared", "adj.r.squared", "nobs"),
+  stars = c('*' = .1, '**' = .05, '***' = 0.01),
+  title = "Task 1d"
+)
 
-# using stargazer as recommended by instructions
-stargazer(TABLE_3, summary = FALSE, type = "text", rownames = TRUE)
-
-
-kable(Table_1, caption = "Table 1") %>%
-  kable_styling(full_width = FALSE, bootstrap_options = c("striped","condensed"))
-kable(TABLE_2, caption = "Table 2") %>%
-  kable_styling(full_width = FALSE, bootstrap_options = c("striped","condensed"))
-kable(TABLE_3, caption = "Table 3") %>%
-  kable_styling(full_width = FALSE, bootstrap_options = c("striped","condensed"))
-
-# we're gonna wanna use latex (i think). then, we have the following latex-transformed output
-# to do that, just change type = "latex" in the stargazer function argument, and
-# copy-paste the output in latex
-
+# The removal of the top/bottom 3, 5, and 10 influential observations reduced the treatment coefficient from 1.68 to 1.35 to 1.22 to 1.02 thousand 1982 dollars, respectively.
+# This amounts to a 40% decrease from the baseline to the 10 observation case.
+# Likewise, the SEs for the estimated was also reduced, but to a lesser extent than the treatment coefficients as the significance dropped to only the 5% significance level.
 
 # Task 2
 
