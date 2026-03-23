4abf12e592442c7e62063c86ad35b0cc5c84d216
Pedro Cassandra <pedrocassandra@Pedro-Cassandra.local>
2026-03-23 10:51:40 +0100

    task 5a and 5c (first part)

---
+++ Task5a5c.R
@@ -0,0 +1,20 @@
+####### Task 5a
+
+#A first condition is that for randomized experiments treatment is assigned 
+# independently from the potential outcomes W⊥(Y(0),Y(1)). Further, the 
+#‘functional form of the assignment mechanism is known’ i.e., one fully 
+# understands how treatment is assigned to its units. 
+#Another condition that must hold is that randomness comes only from
+# assignment of treatment and control groups. Whereby, variation in assignment,
+#if an experiment is repeated, would change, while the individuals’ potential 
+# outcomes would be fixed. Lastly, SUTVA must hold, whereby the potential outcomes 
+# for any unit do notvary with the treatments assigned to other units.
+#If these hold, the treatment and control groups are (in expectation) also 
+# balanced in terms of potential outcomes, allowing for the difference in means
+# to recover the ATE.
+
+####### Task 5c
+
+
+
+
