8ab997376b8897a3b6ba63147bda103071004441
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2026-04-08 15:28:43 +0300

    init PS2

--- PSET2_file.R
+++ PSET2_file.R
@@ -1,5 +1,20 @@
-# new file for pset2 
+# R script for Problem Set 2
+# Group composition: Emanuele Artoni (3199617), Pedro Cassandra (3387647), and Arturs Janis Nikitins (3342806)
 
-data <- read.csv("pset_2.csv", sep = ";",)
+# Set wd
+user <- Sys.info()["user"]
+output_dir <- switch(user,
+  "ajnik"="G:/Mans disks/zObsidian/04 Courses/20295 Microeconometrics/Problem Sets/microeconometrics-ps",
+  "erick"="/home/erick/TEMP/",
+  getwd()
+)
+setwd(output_dir)
+
+# Common setup
+source("./setup.R")
+
+# new file for pset2
+
+data <- read.csv("files/pset_2.csv", sep = ";",)
 
 summary(data)
