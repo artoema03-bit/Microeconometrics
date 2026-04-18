5b8a4780a6b4be05f37b291064e556de0b4373e7
Pedro Cassandra <pedrocassandra@Pedro-Cassandra.local>
2026-04-18 23:34:55 +0200

    comments + code

--- PSET2_file.R
+++ PSET2_file.R
@@ -6,7 +6,6 @@ user <- Sys.info()["user"]
 output_dir <- switch(user,
   "ajnik"="G:/Mans disks/zObsidian/04 Courses/20295 Microeconometrics/Problem Sets/microeconometrics-ps",
   "erick"="/home/erick/TEMP/",
-  "pedrocassandra"="/Users/pedrocassandra/Desktop/Bocconi/Microeconometrics/Micro/Microeconometrics",
   getwd()
 )
 setwd(output_dir)
@@ -30,32 +29,35 @@ summary(data)
 
 # (a) 
 # We must use analytic weights
-# We do so, because the  variable (divorce rate per 1,000 people) is a mean 
+# We do so because the dependent variable (divorce rate per 1,000 people) is a mean 
 # computed from state-level populations (stpop) of different sizes.
+
 # Larger states may provide estimates with lower variance, so weighting 
 # by population accounts for heteroskedasticity and ensures that estimates 
 # are representative of the U.S. population.
 
-# Frequency weights are not relevant since the divorce rates are state-year specific
-# so using frequency weight would imply div_rate is the observed at the corresponding
-# stpop number of times, instead of being a mean.
+# Frequency weights are not relevant, since the divorce rates are state-year specific.
+# Using them would imply div_rate would be observed as if each 
+# observation were repeated stpop times
 
-# Probability weights, are necessary when random sampling is involved which rather
-# than the full-population observation as we have in this case
+# Probability weights are necessary when when data come from a random 
+# sample rather than the full population, this is not relevant in this scenario.
 
 # (b) 
 # (i)
 
-data_1 <- data %>% 
-  mutate (year = as.numeric (year) ,
-          TREATED = as.numeric(lfdivlaw >= 1968 & lfdivlaw <= 1988))
+data_1 <- data %>%
+  mutate(
+    TREATED = case_when(
+      lfdivlaw >= 1968 & lfdivlaw <= 1988 ~ 1, lfdivlaw > 1988 | lfdivlaw == 2000 ~ 0, TRUE ~ NA_real_)) %>%
+  filter(!is.na(TREATED))
 
 data_1 <- data_1 %>%
   group_by (year, TREATED) %>%
   summarise (Y = weighted.mean (div_rate, w = stpop, na.rm = TRUE) ,
              .groups = "drop" )
 
-data_differences_1 <- data_coll_1 %>%
+data_differences_1 <- data_1 %>%
   pivot_wider(names_from= TREATED, values_from= Y) %>% 
   mutate(differences = `1` - `0`)
 
@@ -74,6 +76,9 @@ ggplot (data_1, aes (x = year , y = Y ,
         title = "Outcome Trends") + 
   theme_minimal()
 
+# States that adopted the law prior to the 1968-1988 period, were removed from the
+# sample as they were "always treated" not allowing them to be viable for comparison
+# as members of the control group. 
 
 # (ii)
 
@@ -106,10 +111,13 @@ ggplot (data_2, aes ( x = year , y = Y ,
   theme_minimal()
 
 
-# Do your results support the assumption of parallel trends?
-# The graphs support the parallel trends assumption. Concretely, despite consistent higher divorce rates,
-# the evolution of treated states before the reform (for more than two periods) appears to be very similar to that 
-# of the control group. This, suggests that in the absence of treatment, both groups would have likely followed parallel paths. 
+# The graphs show broadly consistent support for the parallel trends assumption. 
+# Concretely, despite consistently higher divorce rates, the evolution of treated states before the 
+# reform appears to be very similar to  the control group. In particular, the difference
+# between the two groups remains relatively stable—fluctuating around a roughly 
+# constant level of about 1.3 (1.4) divorces per 1,000 people in the first graph 
+# (in the second graph) prior to the reform period. This suggests that in the 
+# absence of treatment, both groups would likelyhave followed parallel paths. 
 
 
 # (c)
@@ -120,6 +128,8 @@ data_3 <- data %>%
           UNILATERAL = as.numeric(lfdivlaw >= 1969 & lfdivlaw <= 1973),
           POST = as.numeric(year == 1978),
           POST_UNILATERAL = (POST*UNILATERAL))
+
+# 
 # (i)
 
 regression_1 <- feols(div_rate ~ POST_UNILATERAL + POST, data = data_3, weights = data_3$stpop)
@@ -139,20 +149,20 @@ summary (did_1)
 #      means$div_rate[means$UNILATERAL==0 & means $ POST==0])
 # print (did_2)
 
-#  The pooled OLS regression (i) shows that treated states are associated with 
-#1.55 more divorces per 1,000 people compared to the control states. This is 
-#statistically significant to the 0.1% level- The full DiD specification (ii), 
-# shows the interaction term POSTxUNILATERAL as non-statistically significant 
+# The pooled OLS regression (i) shows that treated states are associated with 
+# 1.70 more divorces per 1,000 people compared to the control states. This is 
+# statistically significant to the 0.1% level The full DiD specification, (ii), 
+#  indicates that the interaction term POST×UNILATERAhas no statistically significant effect
 # impact of the treatment on divorce rates. Including the UNILATERAL regressor,
-# allows for the capturing of the differences between treatment and control
-# groups before the treatment, which was not accounted for the in the pooled OLS
-# in question (i) causing the difference between the two regressions.
-# The graphs done before support this idea, control groups and treatment group 
+# captures differences between treatment and control groups before the treatment
+# which was not accounted for in the pooled OLS
+# in question (i) leading to the differences between the two regressions.
+# The earlier graphs support this idea, control groups and treatment groups 
 # follow similar trends. Further, after reforms, both groups have an increase in 
-# divorce rates per 1000 people, and the gap between the two does not seem to
-# widen; Rather the two move in parallel, explaining the low, non-significant 
-# DiD estimate in the second specification. Highligthing that the estimated 
-# coefficient that arose from the pooled OLS was biased because it was not considering 
+# divorce rates per 1,000 people, and the gap between the two does not seem to
+# widen. Instead, the two move in parallel, explaining the low, non-significant 
+# DiD estimate in the second specification. This highlights that the estimated 
+# coefficient which arose from the pooled OLS was biased because it did not consider
 # baseline differences. 
 
 # (d)
@@ -216,17 +226,17 @@ summary(regression_4)
 
 # All regressions match with table A1 of appendix 
 
-# Regression i), which assumes strict parallell trends between control and
-# treatment groups has a coefficient of  -0.054838 which is not  statistically significant 
-# Regression ii) has a coefficient of  0.476568 which is significant at the 5% level 
-# Regression iii) has a coefficient of  0.334415  significant at 5% level
-# The latter two allow for state-specific linear and quadratic trends and are 
-# testing whether the baseline results are robust to differential pre-existing 
+# Regression i), which assumes strict parallel trends between control and
+# treatment groups, has a coefficient of -0.055 which is not statistically significant. 
+# Regression ii) has a coefficient of 0.476568 which is significant at the 5% level. 
+# Regression iii) has a coefficient of 0.334415 which is significant at the 5% level.
+# Regressions (ii) and (iii) include state-specific linear and quadratic trends and  
+# test whether the baseline results are robust to differential pre-existing 
 # trajectories across states. If the parallel-trends assumption holds, the
-# coefficient should be similar as the one estimated without including the 
-# linear/quadratic trends. However as noted the differences in coefficients across 
-# regressions, especially the first one agains the latter two, suggests that treated 
-# and control groups may have different trends priot to treatment.  This indicates
+# coefficient should be similar to the one estimated without including the 
+# linear/quadratic trends. However, the differences in coefficients across 
+# regressions, especially the first one against the latter two, suggests that treated 
+# and control groups may have different state specific trends prior to treatment. This indicates
 # that the baseline regression may be biased, and that part of the effect captured 
 # in regressions ii and iii) may be derived not from the treatment effect but 
 # from the inclusion of group-specific trends. 
