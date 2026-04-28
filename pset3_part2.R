fdfb6ab1666488b408607890f81076d6cf28d852
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2026-04-28 21:02:06 +0200

    add ps3 files

--- pset3_part2.R
+++ pset3_part2.R
@@ -18,7 +18,7 @@ library(rddensity)
 library(readstata13)
 library(haven)
 
-data <- read.csv("pset_3.csv", sep = ";", )
+data <- read.csv("files/pset_3.csv", sep = ";", )
 
 summary(data)
 ################################################################################
@@ -44,14 +44,14 @@ rdplotdensity(
 # What are we able to conclude from such a test?
 # Is it favorable or against the validity of the rd design?
 
-# Testing whether the running variable X's density jumps at cutoff yields a t-statistic 
-# T = -1.3937, with p-value = 0.16. Then, we fail to reject the hypothesis that density 
+# Testing whether the running variable X's density jumps at cutoff yields a t-statistic
+# T = -1.3937, with p-value = 0.16. Then, we fail to reject the hypothesis that density
 # is continuous at cutoff.
-# It is also possible to note that binomial tests become significant as the window length 
-# increases, a result consistent with the observation that the there are many more 
-# observations to the left of c ((2314 vs 315, also visible from the histogram), i.e. 
+# It is also possible to note that binomial tests become significant as the window length
+# increases, a result consistent with the observation that the there are many more
+# observations to the left of c ((2314 vs 315, also visible from the histogram), i.e.
 # the relevant party is a minor, local force that only wins a minority of elections.
-# Graphically, the rdplotdensity figure also fails to detect any “bunching” of electoral wins 
+# Graphically, the rdplotdensity figure also fails to detect any “bunching” of electoral wins
 # right above the win margin cutoff.
 
 # Together, these results are supportive of the RDD's credibility.
@@ -75,8 +75,8 @@ print(placebo_density_tests)
 
 # Did we find any evidence in favor of the absence of alternative discontinuities?
 
-# As no t-statistic is significant at the 5% level, we again fail to detect any jump by the 
-# running variable in places where it shouldn't, making any treatment discontinuity at cutoff 
+# As no t-statistic is significant at the 5% level, we again fail to detect any jump by the
+# running variable in places where it shouldn't, making any treatment discontinuity at cutoff
 # more credible.
 
 ################################################################################
@@ -114,14 +114,14 @@ summary(rd_results$uniform)
 # Does electing a mayor from an Islamic party has a significant effect on the educational
 # attainment of women? Do results differ significantly for different kernel choices?
 
-# The point estimate is 3.02 with a triangular kernel, and 3.202 with a uniform one; both are 
-# significant at the 1% level, with similar confidence intervals. Thus, we can 
+# The point estimate is 3.02 with a triangular kernel, and 3.202 with a uniform one; both are
+# significant at the 1% level, with similar confidence intervals. Thus, we can
 # conclude that our results are robust to different kernel specifications.
 
-# The positive, statistically significant coefficient allows us to argue in favor of the 
-# hypothesis that electing a mayor from an Islamic party does positively affect the educational 
-# attainment of women. A possible mechanism, as discussed by Meyersson (2014), seems to involve 
-# the Islamic party's greater ability to remove barriers to education and involvement in 
+# The positive, statistically significant coefficient allows us to argue in favor of the
+# hypothesis that electing a mayor from an Islamic party does positively affect the educational
+# attainment of women. A possible mechanism, as discussed by Meyersson (2014), seems to involve
+# the Islamic party's greater ability to remove barriers to education and involvement in
 # civil society for women from poor and religiously conservative backgrounds.
 
 ################################################################################
